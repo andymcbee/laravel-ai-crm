85741370ebf100325f0c7912b76dba2c6f009af9
andrew <armcburn@gmail.com>
2025-02-17 14:24:13 +0800

    create NotePolicy

---
+++ app/app/Policies/NotePolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Account;
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class NotePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->accounts()->exists();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Note $note): bool
+    {
+
+        // any role may view any contact within an account they belong to
+        return $user->accounts()
+            ->where('accounts.id', $note->account_id)
+            ->exists();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, Account $account): bool
+    {
+        return $user->accounts()
+            ->where('accounts.id', $account->id)
+            ->wherePivotIn('role', ['admin', 'moderator'])
+            ->exists();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Note $note): bool
+    {
+
+        return $user->accounts()
+            ->where('accounts.id', $note->account_id)
+            ->wherePivotIn('role', ['admin', 'moderator'])
+            ->exists();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Note $note): bool
+    {
+        return $user->accounts()
+            ->where('accounts.id', $note->account_id)
+            ->wherePivotIn('role', ['admin'])
+            ->exists();
+    }
+}
